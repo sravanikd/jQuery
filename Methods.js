e703e2ed07922c11ee6462e85f737555ebf3254e
DurgaSravani Kapilavai <32623233+sravanikd@users.noreply.github.com>
2018-01-25 16:47:43 -0600

    Create Methods.js

---
+++ Methods.js
@@ -0,0 +1,20 @@
+jQuery Methods:
+
+val() ex: $('input').val(); to show value of input element.
+          $('input').val("sravani"); to change input value. for drop down menus,checkboxes etc
+text() 
+  ex: $("h1").text() -> shows h1 text content
+      can pass values using text() to update text content ex: $('h1').text("New Text!");
+attr() we use this to retrieve or set the value of attribute ex: $('img').css("width");
+        to set width-- $('img').css("width","500px");
+        to change the image.. $('img').attr("src","img link");
+        
+html() to update elements we use this. to view html of one element $('ul).html();
+        to update element content $('ul').html("<li>I hacked your UL!</li><li>I hacked your Lis as well!</li>");
+addClass() 
+removeClass()
+toggleClass()  to an element if we give toggle method and a class inside it, it'll add if the class isnt already there and removes if the class is already there in that element.
+
+
+Methods of jQuery can be found on api.jquery.com
+
